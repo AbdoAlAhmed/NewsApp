02fd30f102a81471c5621ce4bfe55a88d03e5696
AbdoAlAhmed <AbdoAhmed.bzn@gmail.com>
2023-11-13 21:48:46 +0200

    finish second step fragment

--- app/src/main/java/com/abdo/news/MainActivity.kt
+++ app/src/main/java/com/abdo/news/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.abdo.news
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.abdo.news.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        var navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        var navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+
+
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/abdo/news/ui/ArticleFragment.kt
@@ -0,0 +1,26 @@
+package com.abdo.news.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.abdo.news.R
+
+
+class ArticleFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_article, container, false)
+    }
+
+}
